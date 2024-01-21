42b2a15e8a1a21c76ba9c7c3df6aa2962005da2a
raj-aceec <nagarajuboyapally9@gmail.com>
2024-01-21 10:52:35 +0530

    Initial commit

---
+++ java.js
@@ -0,0 +1,31 @@
+var hr=document.getElementById('hour');
+var min=document.getElementById('minutes');
+var sec=document.getElementById('seconds');
+var ampm1=document.getElementById('ampm');
+
+function updateClock()
+{
+    var date=new Date();
+    h= date.getHours();
+    m= date.getMinutes();
+    s= date.getSeconds();
+    ampm2="AM";
+
+    if(h>12)
+    {
+       h=h-12;
+       ampm2="PM";
+    }   
+    h=h<10?"0"+h:h;
+    m=m<10?"0"+m:m;
+    s=s<10?"0"+s:s;
+
+
+    hr.innerText=h;
+    min.innerText=m;
+    sec.innerText=s;
+    ampm1.innerText=ampm2;
+    
+    settimeout(updateClock,1000)
+}
+updateClock();
